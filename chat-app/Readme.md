70cbf841cc94235c2a88d81da26b945bf98e2118
sangeetdahal <sangeetdahal@MacBookPro.lan>
2025-01-20 19:26:58 -0800

    Added docker compose file and modified main.go to make message send via pub sub

--- chat-app/Readme.md
+++ chat-app/Readme.md
@@ -7,3 +7,11 @@ Run command `docker run -p 8080:8080 chat_app`.
 This runs created docker image in previous step chat_app exposed via port 8080 and routed to port 8080. 
 Note that on [main.go](https://github.com/nromerosoftwareengineer/DistributedSystem/blob/main/chat-app/main.go) the application runs on port 8080.
 Also, on a [Dockerfile](https://github.com/nromerosoftwareengineer/DistributedSystem/blob/main/chat-app/Dockerfile) we have expose the port 8080 so that our running container can be accessed.
+
+
+### docker-compose ###
+You can also use the docker compose file to start two chat applications along with redis container. This uses pub/sub model for message delivery
+Run command `docker-compose up --build -d`. This creates the following:
+* Two chat application tagged with named `chat-app-1` and `chat-app-2` exposed externally via 8101 and 8102 respectively
+* One Redis container
+* The application subscribed to channel "chat-message"
--- chat-app/main.go
+++ chat-app/main.go
@@ -1,12 +1,17 @@
-
 package main
 
 import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"github.com/gorilla/websocket"
+	"github.com/redis/go-redis/v9"
 	"log"
 	"net/http"
-
-	"github.com/gorilla/websocket"
+	"os"
 )
+
+var app_context *AppContext
 
 type webSocketHandler struct {
 	upgrader websocket.Upgrader
@@ -18,46 +23,147 @@ type Message struct {
 	Body string `json:"body"`
 }
 
-var userId_websocket_map = make(map[string]*websocket.Conn)
+type AppContext struct {
+	userId_websocket_map map[string]*websocket.Conn
+	redisClient          *redis.Client
+	ctx                  context.Context
+}
 
-func (wsh webSocketHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	userId := r.URL.Query().Get("userId")
-	c, err := wsh.upgrader.Upgrade(w, r, nil)
-	if err != nil {
-		log.Printf("error %s when upgrading connection to websocket", err)
-		return
+func init_app_context() {
+	app_context = &AppContext{
+		userId_websocket_map: make(map[string]*websocket.Conn),
+		ctx:                  context.Background(),
 	}
-	var msg Message
-	userId_websocket_map[userId] = c
-	for {
-		err := c.ReadJSON(&msg)
+	init_redis(app_context)
+}
+
+func (wsh webSocketHandler) upgrade_to_ws(w http.ResponseWriter, r *http.Request) *websocket.Conn {
+	c, err := wsh.upgrader.Upgrade(w, r, nil)
+	if err == nil {
+		return c
+	}
+	log.Printf("error %s when upgrading connection to websocket", err)
+	w.WriteHeader(http.StatusInternalServerError)
+	_, err = w.Write([]byte("Unable to upgrade to websocket"))
+	if err != nil {
+		log.Printf("error %s when trying to return error message to client", err)
+	}
+	return nil
+}
+
+func handle_if_client_closed_connection(userId string, err error, c *websocket.Conn) {
+	if websocket.IsCloseError(err, websocket.CloseNormalClosure) {
+		err := c.Close()
 		if err != nil {
-			if websocket.IsCloseError(err, websocket.CloseNormalClosure) {
-				err := c.Close()
-				if err != nil {
-					return
-				}
-				log.Println("Connection closed by client")
-			} else {
-				log.Println("Error reading JSON:", err)
-			}
-			break
+			log.Printf("error %s when trying to close websocket connectin for userId:%s", userId, err)
+			return
 		}
-		v := userId_websocket_map[msg.To]
-		writeMessage := Message{
-			From: userId,
-			To:   msg.To,
-			Body: msg.Body,
-		}
-		err = v.WriteJSON(writeMessage)
+		log.Println("Connection closed by client")
+	} else {
+		log.Println("Error reading JSON:", err)
 	}
 }
 
+func message_loop_handler(userId string) {
+	var msg Message
+	c := app_context.userId_websocket_map[userId]
+	for {
+		err := c.ReadJSON(&msg)
+		if err != nil {
+			handle_if_client_closed_connection(userId, err, c)
+			break
+		}
+
+		data, err := json.Marshal(msg)
+		if err != nil {
+			log.Printf("Error marshalling message: %v\n", err)
+			continue
+		}
+		err = app_context.redisClient.Publish(app_context.ctx, "chat-message", data).Err()
+		log.Printf("Received message from userId:%s to userId:%s \n", msg.From, msg.To)
+		if err != nil {
+			log.Printf("Error publishing message: %v\n", err)
+			continue
+		}
+	}
+}
+
+func (wsh webSocketHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	userId := r.Header.Get("userId")
+	log.Printf("userId: %s is trying to connect, upgrading to websocket connection\n", userId)
+	c := wsh.upgrade_to_ws(w, r)
+	if c == nil {
+		return
+	}
+	app_context.userId_websocket_map[userId] = c
+	message_loop_handler(userId)
+}
+
 func main() {
+	init_app_context()
+	go startSubscriber(context.Background(), app_context.redisClient, "chat-message")
 	webSocketHandler := webSocketHandler{
 		upgrader: websocket.Upgrader{},
 	}
 	http.Handle("/", webSocketHandler)
 	log.Print("Starting server...")
-	log.Fatal(http.ListenAndServe("0.0.0.0:8080", nil))
+	log.Fatal(http.ListenAndServe("0.0.0.0:8100", nil))
+}
+
+func init_redis(appContext *AppContext) {
+	run_on_container := os.Getenv("IS_CONTAINER_RUN")
+	var address string
+	if run_on_container == "true" {
+		address = "redis:6379"
+		log.Println("Redis running on " + address)
+	} else {
+		log.Println("Redis running on " + address)
+		address = "0.0.0.0:6379"
+	}
+	rdb := redis.NewClient(&redis.Options{
+		Addr: address,
+	})
+
+	pong, err := rdb.Ping(appContext.ctx).Result()
+	if err != nil {
+		log.Fatalf("Could not connect to Redis: %v", err)
+	}
+	fmt.Println("Redis connected:", pong)
+	appContext.redisClient = rdb
+}
+
+func startSubscriber(ctx context.Context, rdb *redis.Client, channel string) {
+	pubsub := rdb.Subscribe(ctx, channel)
+	defer pubsub.Close()
+
+	fmt.Printf("Subscribed to channel: %s\n", channel)
+	for {
+		msg, err := pubsub.ReceiveMessage(ctx)
+		if err != nil {
+			log.Fatalf("Error receiving message: %v\n", err)
+		}
+
+		var message Message
+		err = json.Unmarshal([]byte(msg.Payload), &message)
+		if err != nil {
+			log.Printf("Invalid JSON message: %s\n", msg.Payload)
+			continue
+		}
+
+		v := app_context.userId_websocket_map[message.To]
+
+		if v == nil {
+			continue
+		}
+
+		writeMessage := Message{
+			From: message.From,
+			To:   message.To,
+			Body: message.Body,
+		}
+		err = v.WriteJSON(writeMessage)
+		if err != nil {
+			log.Println("Error writing JSON:", err)
+		}
+	}
 }
